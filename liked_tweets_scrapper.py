12bd2da56aa0b63ea34f836933f4ef4778724b47
auryhs <70147269+auryhs@users.noreply.github.com>
2024-04-20 20:18:27 +0700

    Update liked_tweets_scrapper.py

--- liked_tweets_scrapper.py
+++ liked_tweets_scrapper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import pandas as pd
